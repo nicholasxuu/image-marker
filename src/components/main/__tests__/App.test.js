edd2f6f59f16101bc0b2989794f3ad49a922bc40
Nicholas Xu <shengfeng.xu@hotmail.com>
2017-11-08 09:07:03 +0800

    added enzyme test

--- src/components/main/__tests__/App.test.js
+++ src/components/main/__tests__/App.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable function-paren-newline,prefer-const */
 // Link.react-test.js
 import React from 'react';
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import App from '../App';
 
@@ -11,4 +12,11 @@ test('sample component snapshot', () => {
 
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('CheckboxWithLabel changes the text after click', () => {
+  // Render a checkbox with label in the document
+  const dom = shallow(<App />);
+
+  expect(dom.text()).toEqual('hello world');
 });
